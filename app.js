65c70152ad36a5624413224ea50bfad6c49d6c09
shreyaansjain <shreyaansjain60@gmail.com>
2021-09-27 18:09:15 +0530

    add database

--- app.js
+++ app.js
@@ -1,33 +1,46 @@
-const express= require('express')
-const port=3000;
-const app= express();
+const express = require('express');
+const port = 3000;
+const mongoose = require('mongoose');
+const app = express();
+// define datablase
+const DB ="mongodb+srv://shreyaansjain06:mernwebapp@cluster0.votq7.mongodb.net/mernwebapp?retryWrites=true&w=majority"
+
+// Establishing connection
+mongoose.connect(DB)
+  .then(() => {
+    console.log(`connection successfull`);
+  })
+  .catch((err) => {
+      console.log(err)
+    console.log(`no connection`);
+  });
 
 // Middleware
-// it helps in authentication 
+// it helps in authentication
 // checking before responging to request
-const middleware=(req,res,next)=>{
-    console.log("middleware");
-    next();
-}
+const middleware = (req, res, next) => {
+  console.log('middleware');
+  next();
+};
 
 // Handling get post req
-app.get("/",(req,res)=>{
-    res.send("hello this my home page");
-})
-app.get("/about",middleware,(req,res)=>{
-    res.send("hello this my about page");
-})
-app.get("/contact",(req,res)=>{
-    res.send("hello this my contact page");
-})
-app.get("/signup",(req,res)=>{
-    res.send("hello this my signup page");
-})
-app.get("/signin",(req,res)=>{
-    res.send("hello this my signin page");
-})
+app.get('/', (req, res) => {
+  res.send('hello this my home page');
+});
+app.get('/about', middleware, (req, res) => {
+  res.send('hello this my about page');
+});
+app.get('/contact', (req, res) => {
+  res.send('hello this my contact page');
+});
+app.get('/signup', (req, res) => {
+  res.send('hello this my signup page');
+});
+app.get('/signin', (req, res) => {
+  res.send('hello this my signin page');
+});
 
 // listening on port
-app.listen(port,()=>{
-    console.log(`Server is listening on port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is listening on port:${port}`);
+});
