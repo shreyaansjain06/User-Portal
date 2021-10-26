49afa106684742f7e46008f6b6636b9a25f1749c
shreyaansjain <shreyaansjain60@gmail.com>
2021-10-26 20:24:24 +0530

    add client and react router dom

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import Navbar from './components/Navbar';
+import './App.css';
+import {Route} from 'react-router-dom';
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Login from './components/Login'
+import Signup from './components/Signup'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Route path="/">
+      <Home/>
+    </Route>
+    <Route path="/about">
+      <About/>
+    </Route>
+    <Route path="/contact">
+      <Contact/>
+    </Route>
+    <Route path="/login">
+      <Login/>
+    </Route>
+    <Route path="/signup">
+      <Signup/>
+    </Route>
+    </>
+  );
+}
+
+export default App;
