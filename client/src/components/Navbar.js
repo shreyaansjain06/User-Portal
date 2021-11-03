51c2e33d58b0d3f1b5135b5c20d73671e949629a
shreyaansjain <shreyaansjain60@gmail.com>
2021-11-03 16:51:49 +0530

    add navbar page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
