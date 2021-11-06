1d641821ec6932578bc35341bb0afa32d7103bed
shreyaansjain <shreyaansjain60@gmail.com>
2021-11-06 18:35:59 +0530

    add home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+        
+        <div className="row h-100">
+                <div className="col-md-6 left_side"></div>
+            </div>
+            <div class="position-absolute top-50 start-50 translate-middle text-center">
+                <h2 className="text-primary">Welcome</h2>
+                <h1 className="desc">I am The Mern Developer</h1>
+            </div>
+
+            
+        </>
     )
 }
 
