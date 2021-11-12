719a86ace238abc23680747ad2a49b0b11245adc
shreyaansjain <shreyaansjain60@gmail.com>
2021-11-12 12:15:32 +0530

    add error page

--- client/src/components/ErrorPage.js
+++ client/src/components/ErrorPage.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom'
 const ErrorPage = () => {
     return (
         <>
-             <h1 class="Error_header position-absolute top-50 start-50 translate-middle">
+             <h1 className="Error_header position-absolute top-50 start-50 translate-middle">
                  404
              </h1>
-             <div class="Error_description position-absolute top-50 start-50 translate-middle text-center mt-3 text-uppercase">
+             <div className="Error_description position-absolute top-50 start-50 translate-middle text-center mt-3 text-uppercase">
                 <h1>We are sorry page not found!</h1>
                 <p>
                  The page you are looking for might have been removed had its name changed
