9fb3a5d1a5116c9b5956bc7304489688f18aae32
shreyaansjain <shreyaansjain60@gmail.com>
2021-11-12 12:14:11 +0530

    fix Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="row h-100">
                 <div className="col-md-6 left_side"></div>
             </div>
-            <div class="position-absolute top-50 start-50 translate-middle text-center">
+            <div className="position-absolute top-50 start-50 translate-middle text-center">
                 <h2 className="text-primary">Welcome</h2>
                 <h1 className="desc">I am The Mern Developer</h1>
             </div>
